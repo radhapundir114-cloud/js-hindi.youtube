276ee3ba25e75c16b4c58ccffed2fd044682ef7c
radhapundir114-cloud <radhapundir114@gmail.com>
2026-06-16 14:34:13 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"// treat all JS code as newer version
+
+// alert(3+3) we are using nodejs,not browser
+
+
+console.log(3+3)
+console.log("radha pundir")
+let name="radha"
+let age= 18
+let isloggedIn = false
+
+
+// number => 2to power 53
+// string=>"
+//boolean=>true/false
+// null=>standalone value
+// undefined =>
+// symbol=>uniqe
+// 
+  console.log(typeof undefined);   
+  console.log(typeof  null)
+  console.log(typeof  age)
+  console.log(typeof  Symbol)
+
+
+  
+  
