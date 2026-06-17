13cd76cbc8a4393fdf6b3a300cb8f3a6c91818d8
radhapundir114-cloud <radhapundir114@gmail.com>
2026-06-17 08:09:35 +0000

    03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,10 +14,32 @@ console.log( typeof booleanIsloggedIn)
 console.log(booleanIsloggedIn)
 // 1=>true; 0=>false;
 // ""=>false
-// "radha"=>true
+// "radha"=>trueh
  let someNumber=33
  let stringNumber=String(someNumber)
  console.log(typeof stringNumber)
  console.log(stringNumber)
+
+
+//  -----------------operation-----------------
+let value = 3
+let negvalue= - value
+console.log(negvalue)
+ let str1="hello"
+ let str2= "ansh"
+ let str3= str1+str2
+ console.log(str3);
+ console.log("1" +2);
+ console.log(1+"2");
+ console.log("1"+2+2);
+ console.log(1+2+"2");
+ console.log(+true);
+ console.log(+"");
+ let num1, num2, num3
+ num1= num2= num3= 2+2
+
+ let gameCounter= 100
+ gameCounter++;
+ console.log(gameCounter);
 
 
