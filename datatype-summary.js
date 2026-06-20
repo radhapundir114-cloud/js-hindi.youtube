3b4740b3df2bedcaf0f41272a0448bc803aa33a7
radhapundir114-cloud <radhapundir114@gmail.com>
2026-06-20 05:49:11 +0000

    datatype-summary.js

---
+++ datatype-summary.js
@@ -0,0 +1,31 @@
+// primitive 
+
+// 7 type : String , Number, Boolean, null , undefined, BigInt , Symbol
+
+const score = 100
+const scoreValue = 100.3
+
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId) 
+const BigNumber= 345685432n
+
+const heros = [ "shketiman" , "naagraj", "doga"]
+ let myObj= {
+    name: "radha"
+    age: 22;
+}
+  const myFunction = function(){
+  console.log("hello world");
+  }
+
+
+
+// Reference (Non primitive)
+
+// Array , Object, Function
